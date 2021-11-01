b850ad04c64a5ada822767e300722050e9e96346
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-01 17:34:38 +0000

    Complete Average of Selected Numbers exercise

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,45 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> inputs = new ArrayList<>();
 
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+
+            inputs.add(input);
+        }
+
+        System.out.println("\nPrint the average of the negative numbers or the positive numbers? (n/p)");
+        String userChoice = scanner.nextLine();
+
+        switch (userChoice) {
+            case "p":
+                double averageOfPositives = inputs.stream()
+                        .mapToInt(num -> Integer.parseInt(num))
+                        .filter(num -> num > 0)
+                        .average()
+                        .getAsDouble();
+                System.out.println("Average of the positive numbers: " + averageOfPositives);
+                break;
+            case "n":
+                double averageOfNegatives = inputs.stream()
+                        .mapToInt(num -> Integer.parseInt(num))
+                        .filter(num -> num < 0)
+                        .average()
+                        .getAsDouble();
+                System.out.println("Average of the negative numbers: " + averageOfNegatives);
+                break;
+        }
     }
+
 }
