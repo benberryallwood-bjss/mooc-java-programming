4cf1af24157023cd8554353b4d5844082d680809
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-01 17:13:00 +0000

    Complete Animals exercise

---
+++ mooc-java-programming-ii/part09-Part09_12.Animals/src/main/java/Cat.java
@@ -0,0 +1,18 @@
+public class Cat extends Animal implements NoiseCapable {
+    public Cat(String name) {
+        super(name);
+    }
+
+    public Cat() {
+        super("Cat");
+    }
+
+    @Override
+    public void makeNoise() {
+        purr();
+    }
+
+    public void purr() {
+        System.out.println(getName() + " purrs");
+    }
+}
