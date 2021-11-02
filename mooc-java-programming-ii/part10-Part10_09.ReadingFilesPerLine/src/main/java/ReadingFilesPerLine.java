3fec660cf76a14ad0c9ff15ba16a13da50d2eed9
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-02 11:11:00 +0000

    Complete Reading Files Per Line exercise

--- mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -14,4 +14,15 @@ public class ReadingFilesPerLine {
 
     }
 
+    public static List<String> read(String file) {
+        List<String> lines = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file))
+                    .forEach(line -> lines.add(line));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return lines;
+    }
+
 }
