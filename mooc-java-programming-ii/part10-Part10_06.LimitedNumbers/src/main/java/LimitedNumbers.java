05fac7b1afd3c02094856b6ab236e521eca84a6d
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-02 10:42:01 +0000

    Complete Limited Numbers exercise

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,24 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Integer> inputs = new ArrayList<>();
 
+        while (true) {
+            int input = Integer.parseInt(scanner.nextLine());
+            if (input < 0) {
+                break;
+            }
+            inputs.add(input);
+        }
+
+        inputs.stream()
+                .filter(num -> 1 <= num && num <= 5)
+                .forEach(System.out::println);
     }
 }
