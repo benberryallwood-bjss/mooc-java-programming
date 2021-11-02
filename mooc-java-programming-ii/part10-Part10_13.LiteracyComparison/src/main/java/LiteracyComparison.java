aaca5d90ce5e5dbcd23b01d67d21bf01aad3a374
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-02 15:06:53 +0000

    Complete Literacy Comparison exercise

--- mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ mooc-java-programming-ii/part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class LiteracyComparison {
     
     public static void main(String[] args) {
-
+        try {
+            Files.lines(Paths.get("literacy.csv"))
+                    .map(line -> line.split(","))
+                    .sorted((line1, line2) -> Double.compare(Double.parseDouble(line1[5].trim()), Double.parseDouble(line2[5].trim())))
+                    .map(parts -> parts[3] + " (" + parts[4] + "), " + parts[2].trim().split(" ")[0] + ", " + parts[5])
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            return;
+        }
     }
 }
