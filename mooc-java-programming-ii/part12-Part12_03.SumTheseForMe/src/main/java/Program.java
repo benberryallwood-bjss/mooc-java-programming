424ec30678ced46b2bbf1cc9ac1b075a9e7a2e1a
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-04 15:47:34 +0000

    Complete Sum These for Me exercise

--- mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -9,4 +9,17 @@ public class Program {
 
     }
 
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        if (fromWhere < 0) fromWhere = 0;
+        if (toWhere >= array.length) toWhere = array.length - 1;
+
+        int sum = 0;
+        for (int i = fromWhere; i <= toWhere; i++) {
+            if (array[i] >= smallest && array[i] <= largest) {
+                sum += array[i];
+            }
+        }
+        return sum;
+    }
+
 }
