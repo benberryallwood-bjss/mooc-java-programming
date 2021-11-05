c5ff023afa6d30827718d236f087fa34c1ce70b2
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-05 13:26:06 +0000

    Complete Die exercise

--- mooc-java-programming-ii/part12-Part12_07.Die/src/main/java/Die.java
+++ mooc-java-programming-ii/part12-Part12_07.Die/src/main/java/Die.java
@@ -7,13 +7,11 @@ public class Die {
     private int numberOfFaces;
 
     public Die(int numberOfFaces) {
-        this.random = new Random();
-        // Initialize the value of numberOfFaces here
+        this.numberOfFaces = numberOfFaces;
+        random = new Random();
     }
 
     public int throwDie() {
-        // generate a random number which may be any number
-        // between one and the number of faces, and then return it
-        return 0;
+        return random.nextInt(numberOfFaces) + 1;
     }
 }
