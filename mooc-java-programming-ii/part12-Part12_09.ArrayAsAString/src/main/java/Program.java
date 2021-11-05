61fd5a6ae0aab26d84be3fd838fa70e4d71e5a56
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-05 13:51:30 +0000

    Complete Array as a String exercise

--- mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -5,4 +5,15 @@ public class Program {
         // Test your method here
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder output = new StringBuilder();
+        for (int[] row : array) {
+            for (int i : row) {
+                output.append(i);
+            }
+            output.append("\n");
+        }
+        return output.toString();
+    }
+
 }
