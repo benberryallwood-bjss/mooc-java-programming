967a2d229c6175f5811bfb1f26997a6c3c4af3f2
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-08 15:07:02 +0000

    Complete Border Pane exercise

--- mooc-java-programming-ii/part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ mooc-java-programming-ii/part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,30 @@
 package borderpane;
 
 
-public class BorderPaneApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class BorderPaneApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(BorderPaneApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+
+        borderPane.setTop(new Label("NORTH"));
+        borderPane.setRight(new Label("EAST"));
+        borderPane.setBottom(new Label("SOUTH"));
+
+        Scene scene = new Scene(borderPane);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 }
