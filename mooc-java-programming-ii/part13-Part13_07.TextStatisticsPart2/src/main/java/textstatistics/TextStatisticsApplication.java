805790ecb5b2f3725447de4a72c0389d194b2467
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-08 16:10:16 +0000

    Complete Text Statistics Part 2 exercise

--- mooc-java-programming-ii/part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ mooc-java-programming-ii/part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,55 @@
 package textstatistics;
 
 
-public class TextStatisticsApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class TextStatisticsApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane layout = new BorderPane();
+
+        TextArea userInput = new TextArea("");
+
+        Label letterCount = new Label("Letters: 0");
+        Label wordCount = new Label("Words: 1");
+        Label longestWord = new Label("The longest word is: ");
+
+        userInput.textProperty().addListener((change, oldValue, newValue) -> {
+            int numOfChars = newValue.length();
+            String[] words = newValue.split(" ");
+            int numOfWords = words.length;
+            String longest = Arrays.stream(words)
+                    .min((s1, s2) -> s2.length() - s1.length())
+                    .get();
+            letterCount.setText("Letters: " + numOfChars);
+            wordCount.setText("Words: " + numOfWords);
+            longestWord.setText("The longest word is: " + longest);
+        });
+
+        HBox textComponents = new HBox();
+        textComponents.setSpacing(10);
+        textComponents.getChildren().addAll(letterCount, wordCount, longestWord);
+
+        layout.setCenter(userInput);
+        layout.setBottom(textComponents);
+
+        Scene scene = new Scene(layout);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 }
