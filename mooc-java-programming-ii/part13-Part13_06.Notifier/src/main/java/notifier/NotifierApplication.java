7f9c68816152bbd966a87e91bc5a8cb0b69c5f18
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-08 15:31:45 +0000

    Complete Notifier exercise

--- mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,38 @@
 package notifier;
 
 
-public class NotifierApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class NotifierApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        TextField userInput = new TextField("");
+        Button copyButton = new Button("Update");
+        Label copiedText = new Label("");
+
+        copyButton.setOnAction((event) -> {
+            copiedText.setText(userInput.getText());
+        });
+
+        VBox componentGroup = new VBox();
+        componentGroup.setSpacing(10);
+        componentGroup.getChildren().addAll(userInput, copyButton, copiedText);
+
+        Scene viewport = new Scene(componentGroup);
+
+        stage.setScene(viewport);
+        stage.show();
+    }
 }
