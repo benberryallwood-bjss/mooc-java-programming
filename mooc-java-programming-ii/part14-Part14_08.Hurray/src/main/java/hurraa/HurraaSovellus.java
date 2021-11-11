bc872feb60599f528ef2b9877a4c6df3991910a0
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-11 14:28:48 +0000

    Complete Hurray exercise

--- mooc-java-programming-ii/part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
+++ mooc-java-programming-ii/part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.media.AudioClip;
 
 public class HurraaSovellus extends Application {
 
@@ -12,8 +13,14 @@ public class HurraaSovellus extends Application {
     public void start(Stage stage) throws Exception {
         BorderPane pane = new BorderPane();
 
-        Button nappi = new Button("Hurraa!");
+        AudioClip applause = new AudioClip("file:Applause-Yannick_Lemieux.wav");
+
+        Button nappi = new Button("Hurray!");
         pane.setCenter(nappi);
+
+        nappi.setOnAction((event) -> {
+            applause.play();
+        });
 
 
         Scene scene = new Scene(pane, 600, 400);
