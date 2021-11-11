1b2987c561398ab95d16f9b22b45b4b97e1ea625
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-11 11:41:12 +0000

    Complete Smiley exercise

--- mooc-java-programming-ii/part14-Part14_06.Smiley/src/main/java/smiley/SmileyApplication.java
+++ mooc-java-programming-ii/part14-Part14_06.Smiley/src/main/java/smiley/SmileyApplication.java
@@ -1,11 +1,36 @@
 package smiley;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class SmileyApplication {
-
+public class SmileyApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(SmileyApplication.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane layout = new BorderPane();
+        Canvas canvas = new Canvas(640, 480);
+
+        GraphicsContext painter = canvas.getGraphicsContext2D();
+        painter.setFill(Color.BLACK);
+
+        painter.fillRect(160, 80, 80, 80);
+        painter.fillRect(360, 80, 80, 80);
+        painter.fillRect(80, 240, 80, 80);
+        painter.fillRect(440, 240, 80, 80);
+        painter.fillRect(160, 320, 280, 80);
+
+        layout.setCenter(canvas);
+        stage.setScene(new Scene(layout));
+        stage.show();
     }
 
 }
