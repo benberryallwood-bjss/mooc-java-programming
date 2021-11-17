b9809724c727e7fd5315cbca75a3dcea272a854c
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-11-17 10:40:57 +0000

    Complete Database exercise

--- mooc-java-programming-ii/part14-Part14_10.Database/src/main/java/application/UserInterface.java
+++ mooc-java-programming-ii/part14-Part14_10.Database/src/main/java/application/UserInterface.java
@@ -30,9 +30,65 @@ public class UserInterface {
             }
 
             // implement the functionality here
+            switch (command) {
+                case "1":
+                    list();
+                    break;
+                case "2":
+                    add();
+                    break;
+                case "3":
+                    markAsDone();
+                    break;
+                case "4":
+                    remove();
+                    break;
+            }
         }
 
         System.out.println("Thank you!");
+    }
+
+    private void list() {
+        System.out.println("Listing the database contents");
+        try {
+            database.list().forEach(System.out::println);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void add() {
+        System.out.println("Adding a new todo");
+        System.out.println("Enter name");
+        String name = scanner.nextLine();
+        System.out.println("Enter description");
+        String description = scanner.nextLine();
+        try {
+            database.add(new Todo(name, description, false));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void markAsDone() {
+        System.out.println("Which todo should be marked as done (give the id)?");
+        int idToMarkAsDone = Integer.parseInt(scanner.nextLine());
+        try {
+            database.markAsDone(idToMarkAsDone);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void remove() {
+        System.out.println("Which todo should be removed (give the id)?");
+        int idToRemove = Integer.parseInt(scanner.nextLine());
+        try {
+            database.remove(idToRemove);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
 }
